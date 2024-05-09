dca256f5c893d5bed77c881a78a95a1cfc6442b7
OussEzza <ezzahrioussama01@gmail.com>
2024-05-09 09:08:24 +0100

    Added AgentTraining controller, model and migration

---
+++ app/Http/Controllers/AgentTrainingController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\AgentTraining;
+use Illuminate\Http\Request;
+
+class AgentTrainingController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\AgentTraining  $agentTraining
+     * @return \Illuminate\Http\Response
+     */
+    public function show(AgentTraining $agentTraining)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\AgentTraining  $agentTraining
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(AgentTraining $agentTraining)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\AgentTraining  $agentTraining
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, AgentTraining $agentTraining)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\AgentTraining  $agentTraining
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(AgentTraining $agentTraining)
+    {
+        //
+    }
+}
